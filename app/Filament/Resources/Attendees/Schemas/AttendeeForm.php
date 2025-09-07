15196960fef106c2f0a1bbc016bf906a9109131b
Dev-Ibrahim2019 <ialashqar.dev64@gmail.com>
2025-09-07 18:55:50 +0300

    feat: use plugins

--- app/Filament/Resources/Attendees/Schemas/AttendeeForm.php
+++ app/Filament/Resources/Attendees/Schemas/AttendeeForm.php
@@ -6,6 +6,8 @@ use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Toggle;
 use Filament\Schemas\Schema;
+use Awcodes\Shout\Components\Shout;
+use Filament\Schemas\Components\Utilities\Get;
 
 class AttendeeForm
 {
@@ -13,6 +15,16 @@ class AttendeeForm
     {
         return $schema
             ->components([
+                Shout::make('warn-price')
+                    ->visible(function (Get $get) {
+                        return $get('ticket_cost') > 500;
+                    })
+                    ->columnSpanFull()
+                    ->type('warning')
+                    ->content(function (Get $get) {
+                        $price = $get('ticket_cost');
+                        return 'This is ' . $price - 500 . ' more than the average ticket price';
+                    }),
                 TextInput::make('name')
                     ->required(),
                 TextInput::make('email')
@@ -20,6 +32,7 @@ class AttendeeForm
                     ->email()
                     ->required(),
                 TextInput::make('ticket_cost')
+                    ->lazy()
                     ->required()
                     ->numeric(),
                 Toggle::make('is_paid')
